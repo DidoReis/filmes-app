ac1ead42b92ab4216d1183ca4c8c9d4d72324e88
DidoReis <djdidoreis@gmail.com>
2022-08-23 12:29:37 -0300

    final config

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+import { RiMovie2Fill } from "react-icons/ri";
+import "./Navbar.css";
+
+function Navbar() {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!search) return;
+    navigate(`/search?q=${search}`);
+    setSearch("");
+  };
+  return (
+    <nav id="navbar">
+      <h2>
+        <Link to="/">
+          <RiMovie2Fill /> Home
+        </Link>
+      </h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Search for a movie"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
+        <button type="submit">
+          <BiSearchAlt2 />
+        </button>
+      </form>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import MovieCard from "../Components/MovieCard";
+
+import "../Components/MovieGrid.css";
+
+const moviesUrl = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+function Home() {
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovie = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setTopMovies(data.results);
+  };
+
+  useEffect(() => {
+    const topRatedUrl = `${moviesUrl}top_rated?${apiKey}`;
+
+    getTopRatedMovie(topRatedUrl);
+  }, []);
+
+  return (
+    <div className="container">
+      <h2 className="title">Melhores filmes:</h2>
+      <div className="movies-container">
+        {topMovies.length === 0 && <p>Carregando...</p>}
+        {topMovies.length > 0 &&
+          topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
